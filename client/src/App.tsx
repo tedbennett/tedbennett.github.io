b835fe26d9a7a662d68b4c503b9b226f8149dbf4
Ted <teddsbennett@gmail.com>
2020-09-09 12:48:06 +0200

    test frontend api calls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Initialize state
+  state = { moo: '' };
+
+  // Fetch moo after first mount
+  componentDidMount() {
+    this.getMoo();
+  }
+
+  getMoo = () => {
+    // Get the moo and store them in state
+    fetch('/api/cow')
+      .then(res => res.text())
+      .then(moo => this.setState({ moo }));
+  };
+
+  render() {
+    const { moo } = this.state;
+
+    return (
+      <div className="App">
+        {/* Render the moo if we have them */}
+        {moo.length ? (
+          <div>
+            <h1>1 Cow.</h1>
+            <h2>{moo}</h2>
+          </div>
+        ) : (
+            // Render a helpful message otherwise
+            <div>
+              <h1>No moo :(</h1>
+            </div>
+          )}
+      </div>
+    );
+  }
 }
 
 export default App;
