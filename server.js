c30edf33726658afbe315f2b1c4c11f346cb15d4
Ted <teddsbennett@gmail.com>
2020-09-09 12:32:05 +0200

    Remove react, rework server

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
+// const cors = require('cors');
 const mongoose = require('mongoose');
-const helmet = require('helmet');
-const morgan = require('morgan');
+// const helmet = require('helmet');
+// const morgan = require('morgan');
 
 require('dotenv').config();
 
 const app = express();
 
-app.use(helmet());
-app.use(morgan('tiny'));
-app.use(cors());
+// app.use(helmet());
+// app.use(morgan('tiny'));
+// app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/cow', (req, res) => {
+  res.send("Moo")
+})
+
+const ideasRouter = require('./routes/ideas');
+const blogsRouter = require('./routes/blogs');
+
+app.use('/api/ideas', ideasRouter);
+app.use('/api/blogs', blogsRouter);
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -25,14 +39,8 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully.');
 });
 
-const ideasRouter = require('./routes/ideas');
-const blogsRouter = require('./routes/blogs');
-
-app.use('/api/ideas', ideasRouter);
-app.use('/api/blogs', blogsRouter);
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
+//   // eslint-disable-next-line no-console
   console.log(`Listening at http://localhost:${port}`);
 });
